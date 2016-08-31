b841168e2b363de0a087409a6c46510bda4c05b8
sivagurukk <sivagurukkd@gmail.com>
2016-08-31 19:19:53 +0530

    Create strtorev.java

---
+++ strtorev.java
@@ -0,0 +1,16 @@
+public class strtorev
+{
+  public static void main (String[] args)
+  {
+  
+    String s = "100";
+
+     the String to int conversion happens here
+      int i = Integer.parseInt(s);
+
+     
+      System.out.println("int i = " + i);
+   
+   
+  }
+}
