17fa6dae9aa7d83536c2b712baf89142eb021bd5
sivagurukk <sivagurukkd@gmail.com>
2016-09-02 19:09:18 +0530

    Update pangram.c

--- pangram.c
+++ pangram.c
@@ -1,26 +1,31 @@
-void main()
-{
-int i,j,index;
-char ch,a[26]=0;
-while((ch=(getchar))!='\n')
-{
-if('A'<=ch&&ch>='Z')
-index=ch-'A';
-elseif('a'<=ch&&ch>='z')
-index=ch-'a';
-else
-continue;
-x[index]=1;
-}
-j=1;
-for(i=0;i<26;++i)
-{
-if(x[i]==0)
-j=0;
-}
-if(j==1)
-printf("It is a pangram");
-else
-printf("It is not a pangram");
-getch();
+int main() {
+ 
+    int len,hash[26],u=0,i;
+ char str[MAX];
+ gets(str);
+ len=strlen(str);
+ memset(hash,0,sizeof(hash));
+ for(i=0;i<len;i++)
+ {
+  if(str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z')
+   {
+    if(islower(str[i]))
+    {
+     if(hash[str[i]-'a'] == 0)
+     u++;
+     hash[str[i]-'a']=1;
+    }
+    else
+    {
+     if(hash[str[i]-'A'] == 0)
+     u++;
+     hash[str[i]-'A']=1;
+    }
+   }
+ }
+ if(u==26)
+ printf("pangram");
+ else
+ printf("not pangram");    
+    return 0;
 }
